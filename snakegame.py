9ed29795b5bb6187496390f6201e0a9d02f4a051
Tyler Hoh <tylerhoh@gmail.com>
2019-06-26 20:13:55 -0700

    +snake body segments +snake food
    need to add borders to frame!
    implement a pause button?

--- snakegame.py
+++ snakegame.py
@@ -3,23 +3,112 @@
 
 # Part 1: building a frame
 import turtle
+import time
+import random
+
+delay = 0.1
 
 window = turtle.Screen()
 window.title("Snake Game by @Tlerhoh")
-window.bgcolor("blue")
+window.bgcolor("green")
 window.setup(width=600, height=600)
-window.tracer(0)//turns off screen updates
+window.tracer(0)  # turns off screen updates
 
-#Snake Head
+# Snake Head
 head = turtle.Turtle()
 head.speed(0)
 head.shape("square")
 head.color("black")
 head.penup()
-head.goto(0,0)
+head.goto(0, 0)
 head.direction = "stop"
 
-#main game loop
+segments = []
+# Snake Food
+food = turtle.Turtle()
+food.speed(0)
+food.shape("circle")
+food.color("red")
+food.penup()
+food.goto(0, 0)
+
+
+# Functions
+
+def go_up():
+    head.direction = "up"
+
+
+def go_down():
+    head.direction = "down"
+
+
+def go_left():
+    head.direction = "left"
+
+
+def go_right():
+    head.direction = "right"
+
+
+def move():
+    if head.direction == "up":
+        y = head.ycor()
+        head.sety(y + 20)
+
+    if head.direction == "down":
+        y = head.ycor()
+        head.sety(y - 20)
+
+    if head.direction == "left":
+        x = head.xcor()
+        head.setx(x - 20)
+
+    if head.direction == "right":
+        x = head.xcor()
+        head.setx(x + 20)
+
+
+# keyboard bindings
+window.listen()
+
+window.onkeypress(go_up, "w")
+window.onkeypress(go_down, "s")
+window.onkeypress(go_left, "a")
+window.onkeypress(go_right, "d")
+# main game loop
+
 while True:
     window.update()
+
+    if head.distance(food) < 20:
+        # move food to random spot
+        x = random.randint(-290, 290)
+        y = random.randint(-290, 290)
+        food.goto(x, y)
+
+        # add a segment
+        new_segment = turtle.Turtle()
+        new_segment.speed(0)
+        new_segment.shape("square")
+        new_segment.color("black")
+        new_segment.penup()
+        segments.append(new_segment)
+
+    # Move the end segments first in reverse order
+    for index in range(len(segments) - 1, 0, -1):
+        x = segments[index - 1].xcor()
+        y = segments[index - 1].ycor()
+        segments[index].goto(x, y)
+
+    # move Segment 0 to where the head is
+    if len(segments) > 0:
+        x = head.xcor()
+        y = head.ycor()
+        segments[0].goto(x, y)
+
+    move()
+
+    time.sleep(delay)
+
 window.mainloop()
