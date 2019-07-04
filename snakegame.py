0f22f979c1b8ba00a01d70cd664738cd7f658fcf
Tyler Hoh <tylerhoh@gmail.com>
2019-07-03 18:54:22 -0700

    +BorderCheck: if hit borders user loses game and starts over +scoreboard: Score and High Score displayed at top. Score resets when user loses

--- snakegame.py
+++ snakegame.py
@@ -7,6 +7,9 @@ import time
 import random
 
 delay = 0.1
+# score
+score = 0
+high_score = 0
 
 window = turtle.Screen()
 window.title("Snake Game by @Tlerhoh")
@@ -24,6 +27,16 @@ head.goto(0, 0)
 head.direction = "stop"
 
 segments = []
+
+# Pen
+pen = turtle.Turtle()
+pen.speed(0)
+pen.shape("square")
+pen.color("white")
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Score: 0 High Score:0", align="center", font=("Courier", 24, "normal"))
 # Snake Food
 food = turtle.Turtle()
 food.speed(0)
@@ -32,17 +45,25 @@ food.color("red")
 food.penup()
 food.goto(0, 0)
 
+
+def checkBorders():
+    global score
+    global segment
+    if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290 or head.ycor() < -290:
+        time.sleep(1)
+        head.goto(0, 0)
+        head.direction = "stop"
+
+        # hide segments
+        for segment in segments:
+            segment.goto(1000, 1000)
+        # clear segments
+        segments.clear()
+
+        # Reset the score
+        score = 0;
+
 # Check for collisions with borders
-if head.xcor() > 290 or head.xcor() < -290 or head.ycor() > 290:
-    time.sleep(1)
-    head.goto(0, 0)
-    head.direction = "stop"
-
-    # hide segements
-    for segment in segments:
-        segment.goto(1000, 1000)
-
-    segments.clear()
 
 
 # Functions
@@ -107,6 +128,14 @@ while True:
         new_segment.penup()
         segments.append(new_segment)
 
+        # Increase the score
+        score += 10
+        if score > high_score:
+            high_score = score
+
+        pen.clear()
+        pen.write("Score:  {}  High Score:  {}".format(score, high_score), align="center", font=("Courier", 24, "normal"))
+
     # Move the end segments first in reverse order
     for index in range(len(segments) - 1, 0, -1):
         x = segments[index - 1].xcor()
@@ -120,6 +149,17 @@ while True:
         segments[0].goto(x, y)
 
     move()
+    checkBorders()
+    for segment in segments:
+        if segment.distance(head) < 20:
+            time.sleep(1)
+            head.goto(0, 0)
+            head.direction = "stop"
+
+            # Hide the segments
+            for segment in segments:
+                segment.goto(1000, 1000)
+            segments.clear()
 
     time.sleep(delay)
 
